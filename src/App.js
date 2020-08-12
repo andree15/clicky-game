5c94963368633d7246b396eb6e65539bfc7945bb
Andy Edstrom <andyedstrom115@gmail.com>
2020-08-12 11:50:04 -0400

    finished app

--- src/App.js
+++ src/App.js
@@ -5,18 +5,61 @@ import Header from "./components/Header";
 import characters from './characters.json';
 import './App.css';
 
+class App extends Component {
 
-export default class App extends Component {
-  return (
-    <Wrapper>
-      <h1 className="title">Characters List</h1>
-      {characters.map = ((character) => (
-        <CharacterCard
-          image={character.image}
-        />
-      ))}
-    </Wrapper>
-  )
+  state = {
+    characters,
+    score: 0,
+    highscore: 0,
+  };
+
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+        console.log(this.state.highscore);
+      })
+    }
+    this.state.characters.forEach(character => {
+      character.count = 0;
+    });
+    alert(`Game Over:( | score: ${this.state.score}`);
+    this.setState({score:0});
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.characters.find((o, i) => {
+      if (o.id === id) {
+        if(characters[i].count === 0){
+          characters[i].count = characters[i].count + 1;
+          this.setState({score : this.state.score + 1}, function() {
+            console.log(this.state.score)
+          });
+          this.state.characters.sort(() => Math.random() - 0.5)
+          return true;
+        }
+        else{
+          this.gameOver();
+        }
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Header score = {this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        {this.state.characters.map(character => (
+          <Card
+            clickCount={this.clickCount}
+            id={character.id}
+            key={character.id}
+            image={character.image}
+            />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
