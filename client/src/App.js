ce6b3351985a5771c870a32031b74bbfc4c2b515
Andy Edstrom <andyedstrom115@gmail.com>
2020-08-17 22:23:00 -0400

    added server.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,63 +2,59 @@ import React, { Component } from "react";
 import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-import characters from './characters.json';
-import './App.css';
+import characters from "./characters.json";
+import "./App.css";
 
 class App extends Component {
-
+  // Setting this.state.characters to the characters json array
   state = {
     characters,
     score: 0,
-    highscore: 0,
+    highscore: 0
   };
 
-  //for when user choses the same character twice
   gameOver = () => {
     if (this.state.score > this.state.highscore) {
       this.setState({highscore: this.state.score}, function() {
         console.log(this.state.highscore);
-      })
+      });
     }
-    this.state.characters.forEach(character => {
-      character.count = 0;
+    this.state.characters.forEach(card => {
+      card.count = 0;
     });
-    alert(`Game Over:( | score: ${this.state.score}`);
-    this.setState({score:0});
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
     return true;
   }
-
 
   clickCount = id => {
     this.state.characters.find((o, i) => {
       if (o.id === id) {
         if(characters[i].count === 0){
           characters[i].count = characters[i].count + 1;
-          this.setState({score : this.state.score + 1}, function() {
-            console.log(this.state.score)
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
           });
           this.state.characters.sort(() => Math.random() - 0.5)
-          return true;
-        }
-        else{
+          return true; 
+        } else {
           this.gameOver();
         }
       }
     });
   }
-
-  // Rendered HTML and CSS
+  // Map over this.state.characters and render a cardCard component for each card object
   render() {
     return (
       <Wrapper>
-        <Header score = {this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
-        {this.state.characters.map(character => (
+        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        {this.state.characters.map(card => (
           <Card
             clickCount={this.clickCount}
-            id={character.id}
-            key={character.id}
-            image={character.image}
-            />
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
         ))}
       </Wrapper>
     );
